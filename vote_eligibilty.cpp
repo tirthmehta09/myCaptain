8e5cf5cd2ecaaffc1ac076c4fdb180cca9676c1d
tirthmehta09 <89314063+tirthmehta09@users.noreply.github.com>
2021-08-21 23:20:05 +0530

    Add files via upload

---
+++ vote_eligibilty.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int age;
+    cout << "Enter age of a user:";
+    cin>>age;
+
+    if (age >= 18) {
+        cout << "You are eligible for voting";
+        return 0;
+    }
+    
+	cout << "You are not eligible for voting";
+    return 0;
+}
+
+
